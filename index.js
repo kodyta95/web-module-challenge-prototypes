287f5a209b7eda3182637f13cc06a67b21508953
kody ta <kodyta95@gmail.com>
2022-12-27 15:53:01 -0500

    finished prototype module

--- index.js
+++ index.js
@@ -15,9 +15,29 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function (){
+  this.stomach = [];
+}
+Person.prototype.toString = function (){
+  return `${this.name}, ${this.age}`;
+}
+
+// const kody = new Person('Kody', 27);
+// console.log(kody.toString());
+// kody.eat('rice');
+// console.log(kody.stomach);
+// kody.poop();
+// console.log(kody.stomach);
 
 
 /*
@@ -36,8 +56,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
+  const driveableMiles = this.tank * this.milesPerGallon;
+  if(distance <= driveableMiles){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+  }else{
+    this.odometer = this.odometer + driveableMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`;
+  }
 }
 
 
@@ -49,18 +86,27 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+
+  1. window binding - when no rules apply, this will return the window or global object in node or undefined in strict mode.
+
+  2. implicit binding -when the function is invoked look to the left of the dot that it's referring to.
+
+  3. explicit binding - .call, .apply, .bind
+
+  4. new binding - when a function  is created as a constructor this points to the newly created object.
 */
 
 ///////// END OF CHALLENGE /////////
